c4170510eb7b427cc53e379f05005cce636aa0b6
gitsujal01 <sujallokhande23@gmail.com>
2026-03-04 18:37:11 +0530

    added .env file

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const baseUrl = 'chatapp-backend-production-ceef.up.railway.app'; 
+export const baseUrl = 'https://chatapp-backend-production-ceef.up.railway.app';
 export const httpClient = axios.create(
     {
         baseURL: baseUrl,
